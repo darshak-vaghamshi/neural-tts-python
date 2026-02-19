019b2b6714a6cae32a99eb281cc93ec1ada60213
Darshak Vaghamshi <137603689+darshak-vaghamshi@users.noreply.github.com>
2026-02-19 10:28:17 +0530

    Delete audio directory

--- audio/api/languages.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from ..config import SUPPORTED_LANGUAGES
-
-router = APIRouter(prefix="/languages", tags=["languages"])
-
-@router.get("")
-async def get_languages():
-    return {"languages": SUPPORTED_LANGUAGES}
